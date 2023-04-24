42069f857358bdc4aa2118875b79c8974f15b45e
Royce Lando <Roycelando@gmail.com>
2023-04-24 19:02:37 -0400

    latest changes 2023/04/24

--- RayTracer/ray.h
+++ RayTracer/ray.h
@@ -5,7 +5,7 @@
 class Ray {
 		Vector dir; // Ray direction
 		Point origin; // Ray origin
-		double t; // how far along you are on the ray R(t) = Ro + Rd*t; Ro = origin, Rd = dir, t=t
+		double t =-1; // how far along you are on the ray R(t) = Ro + Rd*t; Ro = origin, Rd = dir, t=t
 
 	public:
 		Ray(Vector dir, Point origin) {
